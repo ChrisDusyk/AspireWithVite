4e24789c79d01439c8da85bc819a5105f40c2db8
Chris Dusyk <chris.dusyk@gmail.com>
2025-02-14 10:06:23 -0600

    Run migrations from aspire

--- ViteAspire9.AppHost/Program.cs
+++ ViteAspire9.AppHost/Program.cs
@@ -9,11 +9,17 @@ var postgres = builder
 
 var db = postgres.AddDatabase("resume");
 
+var dbMigrator = builder
+	.AddProject<Projects.ViteAspire9_Database_Migrator>("dbMigrator")
+	.WithReference(db)
+	.WaitFor(db);
+
 var api = builder
 	.AddProject<Projects.ViteAspire9_Api>("api")
 	.WithScalar()
 	.WithReference(db)
-	.WaitFor(db);
+	.WaitFor(db)
+	.WaitFor(dbMigrator);
 
 var ui = builder
 	.AddViteApp("ui", "../ViteAspire9.UI", packageManager: "pnpm")
